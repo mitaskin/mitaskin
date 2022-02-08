d41ea9a2fe4646da46d253816850afc2d82b8640
mitaskin <mi.taskin027@gmail.com>
2022-02-08 14:32:10 +0300

    main

--- callbackornek/function.js
+++ callbackornek/function.js
@@ -1,6 +1,24 @@
-/* let sum = function (x, y) {
+let sum = function (x, y) {
     return x + y
 }
 
-console.log(sum(2, 3)) */
+console.log(sum(2, 3))
 
+
+
+let sum1 = (x, y) => x + y
+
+console.log(sum1(2, 3))
+
+
+
+
+let squared = function (x) {
+    return x * x
+}
+
+console.log(squared(2.23606797749983))
+
+let squared1 = x => x * x
+
+console.log(squared1(2.2))
